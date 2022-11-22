e9dcd87eee1b848db79b8e62f11d58bdbeb37e46
Chris Mems <christopher.mems@.com>
2022-11-22 16:17:08 -0600

    center content in nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,8 +2,12 @@ import "../styles/NavBar.css"
 
 const NavBar = () => {
   return (
-    <div>
-      
+    <div className="nav-bar">
+      <ul className="nav-items">
+        <li>Resources</li>
+        <li>Home</li>
+        <li>Advice</li>
+      </ul>
     </div>
   );
 }
