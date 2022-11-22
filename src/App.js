84c880112744a96155bce747118208e6f3d2cdee
Chris Mems <christopher.mems@.com>
2022-11-22 14:31:27 -0600

    import navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar';
 import Home from './pages/Home';
 import Advice from './pages/Advice';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/advice' element={<Advice />}/>
--- src/pages/Advice.jsx
+++ src/pages/Advice.jsx
@@ -1,7 +1,7 @@
 const Advice = () => {
   return (
     <div>
-
+      
     </div>
   );
 }
