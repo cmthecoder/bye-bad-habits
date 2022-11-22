4a54160e903edd065fac0c7095ca3314c2856121
Chris Mems <christopher.mems@.com>
2022-11-22 14:29:10 -0600

    link pages to app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Advice from './pages/Advice';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/advice' element={<Advice />}/>
+      </Routes>
     </div>
   );
 }
