aedb92b637a3307b048824edda22a3ebbe88ae42
Chris Mems <christopher.mems@.com>
2022-11-25 10:14:22 -0600

    add advice for no more bad habits

--- src/pages/Advice.jsx
+++ src/pages/Advice.jsx
@@ -4,10 +4,26 @@ const Advice = () => {
   return (
     <section className="advice-containter">
       <article className="advice">
-        <p className="advice-content">
-          Advice
-        </p>
-        
+        <div className="avoid">
+          <h2>Avoid tempting situations</h2>
+          <p></p>
+        </div>
+        <div className="replace">
+          <h2>Replace unhealthy behaviors with healthy ones</h2>
+          <p></p>
+        </div>
+        <div className="Prepare">
+          <h2>Prepare mentally</h2>
+          <p></p>
+        </div>
+        <div className="enlist">
+          <h2>Ask for help</h2>
+          <p></p>
+        </div>
+        <div className="reward">
+          <h2>Reward yourself of small steps</h2>
+          <p></p>
+        </div>
       </article>
     </section>
   );
